0511f96f24e254ef18c073897b9273da224e1ff7
borges001 <cleiton.001@icloud.com>
2023-02-04 08:22:44 -0300

    Social links ready

--- components/socialLinks.tsx
+++ components/socialLinks.tsx
@@ -1,4 +1,4 @@
-import { Box, HStack } from "@chakra-ui/react";
+import { Box, HStack, Link } from "@chakra-ui/react";
 import next from "next";
 import { FaInstagram, FaTwitter, FaWhatsapp, FaYoutube } from 'react-icons/fa';
 import { AiOutlineMail } from 'react-icons/ai'
@@ -7,11 +7,27 @@ import { AiOutlineMail } from 'react-icons/ai'
 export default function SocialLinks (){
   return(
     <HStack spacing='15px' justify='center'mt='4' mb='2'>
-      <Box as={ FaWhatsapp } w='40px' h='40px'/>
-      <Box as={ FaInstagram } w='40px' h='40px'/>
-      <Box as={ AiOutlineMail } w='40px' h='40px'/>
-      <Box as={ FaTwitter } w='40px' h='40px'/>
-      <Box as={ FaYoutube } w='40px' h='40px'/>
+
+      <Link href='https://wa.me/5519997608863' isExternal>
+        <Box as={ FaWhatsapp } w='40px' h='40px' />
+      </Link>
+      
+      <Link href='https://instagram.com/cleiton.borgees/' isExternal >
+        <Box as={ FaInstagram } w='40px' h='40px'/>
+      </Link>
+
+      <Link href='mailto:cleiton.borges@digitalbgs.com' isExternal>
+        <Box as={ AiOutlineMail } w='40px' h='40px'/>
+      </Link>
+
+      <Link href='https://twitter.com/cleitonborgees' isExternal>
+        <Box as={ FaTwitter } w='40px' h='40px'/>
+      </Link>
+
+      <Link href='https://youtube.com/@cleiton.borges' isExternal>
+        <Box as={ FaYoutube } w='40px' h='40px'/>
+      </Link>
+
     </HStack>
   )
 }
