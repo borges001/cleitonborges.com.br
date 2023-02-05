a4d5d17db453851521e771d4910e854ee14a569c
borges001 <cleiton.001@icloud.com>
2023-02-05 04:12:30 -0300

    flexDirection fix

--- components/buttonGrid.tsx
+++ components/buttonGrid.tsx
@@ -1,16 +1,15 @@
-import { Box, Flex, Square, Stack, } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import next from "next";
 
 export default function ButtonGrid (){
   return(
-    <Box as='section' w='100%' >
-
-      <Box as='button' bg='#942626' borderRadius='10px' h='85px' w='90%' maxW='100%' mr='20px' ml='20px' >
+    <Flex w='100%' direction='column' gap='2' >
+      <Box as='button' bg='#1C1C1C' borderRadius='10px' h='85px'  mr='20px' ml='20px' >
       </Box>
-      <Box as='button' bg='#942626' borderRadius='10px' h='85px' w='90%' maxW='100%' mr='20px' ml='20px' >
+      <Box as='button' bg='#1C1C1C' borderRadius='10px' h='85px'  mr='20px' ml='20px' >
       </Box>
-      <Box as='button' bg='#942626' borderRadius='10px' h='85px' w='100%' maxW='100%' mr='20px' ml='20px' >
+      <Box as='button' bg='#1C1C1C' borderRadius='10px' h='85px'  mr='20px' ml='20px' >
       </Box>
-    </Box>
+    </Flex>
   )
 }
