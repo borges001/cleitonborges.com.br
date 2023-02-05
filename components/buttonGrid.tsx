f1b255cc3ba1977ddc47a4c4821418c7c597486d
borges001 <cleiton.001@icloud.com>
2023-02-05 04:00:38 -0300

    fix dir

--- components/buttonGrid.tsx
+++ components/buttonGrid.tsx
@@ -3,7 +3,8 @@ import next from "next";
 
 export default function ButtonGrid (){
   return(
-    <Box as='section' w='100%' direction='column'>
+    <Box as='section' w='100%' >
+
       <Box as='button' bg='#942626' borderRadius='10px' h='85px' w='90%' maxW='100%' mr='20px' ml='20px' >
       </Box>
       <Box as='button' bg='#942626' borderRadius='10px' h='85px' w='90%' maxW='100%' mr='20px' ml='20px' >
