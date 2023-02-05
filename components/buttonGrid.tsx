8a9a00bea480634e041175d26b1e92b232d234a3
borges001 <cleiton.001@icloud.com>
2023-02-05 03:53:20 -0300

    new set of buttons

--- components/buttonGrid.tsx
+++ components/buttonGrid.tsx
@@ -1,31 +1,15 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Flex, Square, Stack, } from "@chakra-ui/react";
 import next from "next";
 
 export default function ButtonGrid (){
   return(
-<>
-  <Box 
-  bg='#1C1C1C' 
-  h='85px'
-  borderRadius='10px' 
-  mr='20px'
-  ml='20px'
-
-  >
-    <Box
-    borderRadius='10px' 
-    bg='white'
-    bgImage="url('buttonImg.png')" 
-    bgPosition="center"
-    bgRepeat="no-repeat"
-    backgroundSize= 'contain'
-    h='85px'
-    maxW='170px'
-    >
-      <Box bgGradient='linear(to-r, rgba(28, 28, 28, 0) 50%, #1C1C1C 90%)' h='85px' maxW='170px'></Box>
+    <Box as='section' w='100%' direction='column'>
+      <Box as='button' bg='#942626' borderRadius='10px' h='85px' w='90%' maxW='100%' mr='20px' ml='20px' >
+      </Box>
+      <Box as='button' bg='#942626' borderRadius='10px' h='85px' w='90%' maxW='100%' mr='20px' ml='20px' >
+      </Box>
+      <Box as='button' bg='#942626' borderRadius='10px' h='85px' w='100%' maxW='100%' mr='20px' ml='20px' >
+      </Box>
     </Box>
-    <Text>Máquina de VEndas</Text>
-  </Box>
-</>
-)
+  )
 }
